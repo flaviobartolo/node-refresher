dacc03528f0560ea6b29767eb1130e98d05710c4
flaviobartolo <flavio.bartolo2@gmail.com>
2022-08-29 19:18:25 +0100

    understand express and how it works

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser') // body-parser is a node library that give us ready to use middlewares which we can use in express apps
+
 const app = express()
 
-app.use((req, res, next) => { // app.use() express method to create middlewares
+/* app.use((req, res, next) => { // app.use() express method to create middlewares
   next() // if you are not calling next() then any middleware after this one will not be reached by the request; 
         //  you should always call next() unless you are in the middleware where you wanna send back a response
-}) 
+})  */
 
-app.use((req, res, next) => {
+// requests without body-parser
+/* app.use((req, res, next) => {
   let body = ''
 
   req.on('end', () => {
@@ -21,13 +24,21 @@ app.use((req, res, next) => {
     body += chunk
   })
 
+}) */
+
+// requests with body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res, next) => {
+  console.log('get')
+  res.send('<form action="/user" method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form>')
 })
 
-app.use((req, res, next) => {
-  if (req.body) {
-    return res.send(`<h1>${req.body.name}</h1>`)
-  }
-  res.send('<form method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form>')
+app.post('/user', (req, res, next) => {
+  console.log('post')
+  res.send(`<h1>User: ${req.body.username}</h1>`)
 })
+
+
 
 app.listen(5000)
